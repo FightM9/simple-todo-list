98aaea9be37160192dde931fce5182a618ef5892
FightM9 <fightm9@gmail.com>
2022-03-19 20:47:58 +0300

    Initial Commit

---
+++ js/utils/random.js
@@ -0,0 +1,23 @@
+// declare all characters
+const characters =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+/**
+ * Returns a random string
+ *
+ * @param {Number} length - generated string length
+ *
+ * @return {String}
+ */
+
+function generateString(length = 4) {
+  let result = "";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
+export default generateString;
